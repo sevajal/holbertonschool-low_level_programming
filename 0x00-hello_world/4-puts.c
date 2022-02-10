d618c37af286674270856076e0a233dd86249ce7
sevajal <sevajal@yahoo.com>
2022-02-10 16:45:46 -0500

    I changed the code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-/**
+/*
  * main - Print a string.
  * 
  * Return: 0
  */
-
 int main(void)
 {
 	char myStr[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(myStr);
-}   return (0);
+	return (0);
 }
