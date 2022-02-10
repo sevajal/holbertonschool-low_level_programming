bc2deb4429a59dbd157d72847df857e73092c022
sevajal <sevajal@yahoo.com>
2022-02-10 16:54:41 -0500

    I created the file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Print a string with printf.
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
