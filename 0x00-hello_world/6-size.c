b30b2789fd8de12245e54da7116d09ef02880a98
sevajal <sevajal@yahoo.com>
2022-02-10 17:41:47 -0500

    I changed the code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,20 @@
  */
 int main(void)
 {
-	printf("%lu\n", "Size of a char: " sizeof(char) " byte(s)");
-	printf("%lu\n", "Size of a int: " sizeof(int) " byte(s)");
-	printf("%lu\n", "Size of a long int: " sizeof(long int) " byte(s)");
-	printf("%lu\n", "Size of a long long int: " sizeof(long long int) " byte(s)");
-	printf("%lu\n", "Size of a float: " sizeof(float) " byte(s)");
+	char a;
+
+	int b;
+
+	long int c;
+
+	long long int d;
+
+	float e;
+
+	printf("%lu\n", sizeof(a));
+	printf("%lu\n", sizeof(b));
+	printf("%lu\n", sizeof(c));
+	printf("%lu\n", sizeof(d));
+	printf("%lu\n", sizeof(e));
 	return (0);
 }
