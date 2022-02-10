45f4f50344dde7aa721bb7f442f123d01582d922
sevajal <sevajal@yahoo.com>
2022-02-10 17:53:34 -0500

    I changed the code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,7 +17,7 @@ int main(void)
 
 	float e;
 
-	printf("Size of a char: " sizeof(a) " byte(s))\n";
+	printf("Size of a char: " ("%c\n",sizeof(a)) " byte(s))");
 	printf("%lu\n", sizeof(b));
 	printf("%lu\n", sizeof(c));
 	printf("%lu\n", sizeof(d));
