9b10a331302f5b95c078c33c12df11c6afba700d
sevajal <sevajal@yahoo.com>
2022-02-11 16:02:52 -0500

    I created the file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet.
+ * Return: 0
+ */
+int main(void)
+{
+char letter = 'a';
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	return (0);
+}
