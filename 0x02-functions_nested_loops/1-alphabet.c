431af193d3bc35830af9f5c2350c233319425060
sevajal <sevajal@yahoo.com>
2022-02-14 14:58:05 -0500

    I changed the code

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,13 +3,17 @@
  * main - Prints the alphabet
  * Return: 0
  */
-
-int main(void)
+void print_alphabet(void)
 {
 	char i;
 
 	for (i = 97; i <= 122; i++)
 	_putchar(i);
+}
+
+int main(void)
+{
+	print_alphabet();
 	_putchar(10);
 	return (0);
 }
