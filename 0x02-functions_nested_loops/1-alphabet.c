e638c426ff0c09e5534d710ed1834bb829e2d02f
sevajal <sevajal@yahoo.com>
2022-02-14 12:13:22 -0500

    I created the file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Prints the alphabet
+ * Return: 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 97; i <= 122; i++)
+	_putchar(i);
+	_putchar(10);
+	return (0);
+}
