6a77063eb44cb61c8687a9d042e3dd02de4acc52
sevajal <sevajal@yahoo.com>
2022-02-14 11:13:58 -0500

    I created the main.h and the first file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * main - Prints _putchar
+ * Return: 0
+ */
+int main(void)
+{
+	char _ = '_';
+	char p = 'p';
+	char u = 'u';
+	char t = 't';
+	char c = 'c';
+	char h = 'h';
+	char a = 'a';
+	char r = 'r';
+
+	_putchar(_);
+	_putchar(p);
+	_putchar(u);
+	_putchar(t);
+	_putchar(c);
+	_putchar(h);
+	_putchar(a);
+	_putchar(r);
+	return (0);
+}
