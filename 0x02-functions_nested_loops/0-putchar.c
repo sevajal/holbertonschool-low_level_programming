be4de365c1fffdc7a5bfe1a7cd6d0d7fac197a04
sevajal <sevajal@yahoo.com>
2022-02-15 09:11:41 -0500

    I changed the code

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
 #include "main.h"
+
 /**
  * main - writes the string "_putchar"
- * @c: The character to print
  * Return: 0
  */
-
-int _putchar(char c);
 
 int main(void)
 {
 	char a[] = "_putchar";
 	int i;
 
-	for (i = 1; i <= 7; i++);
-	_putchar(a[i]);
-	_putchar('\n');
+	for (i = 0; i <= 7; i++)
+	{
+		_putchar(a[i]);
+	}
+		_putchar('\n');
 	return (0);
 }
