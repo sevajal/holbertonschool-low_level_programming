061b56ded1fcb0952c83e74537efdaf0fff9d327
sevajal <sevajal@yahoo.com>
2022-02-15 11:56:36 -0500

    I created the files

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - print integer last digit
+ * @c: The int to evaluate
+ * Return: 0
+ */
+
+int print_last_digit(int c)
+{
+	_putchar(c % 10);
+	return (0);
+}
