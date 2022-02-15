7f8ae6c6036bc2f7b04f63a48b0a208131199ea7
sevajal <sevajal@yahoo.com>
2022-02-15 11:37:34 -0500

    I created the files

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - print the absolute value of a integer
+ * @c: The int to evaluate
+ * Return: 0
+ */
+
+int _abs(int c)
+{
+	if (c >= 48)
+		return (c);
+	else
+		return (-(c));
+}
