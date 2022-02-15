f369ac0967afa8c14f4d6a728dde00b7127c2ff4
sevajal <sevajal@yahoo.com>
2022-02-15 09:41:25 -0500

    I created the files

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,22 @@
 #include "main.h"
 /**
- * main - Prints the alphabet 10 times.
+ * print_alphabet_x10 - Prints the alphabet 10 times.
  * Return: 0
  */
 
-int main(void)
+void print_alphabet_x10(void)
 {
-	print_alphabet_x10();
-	return (0);
+	char i = 48;
+
+	while (i <= 57)
+	{
+	char j;
+
+	for (j = 97; j <= 122; j++)
+	{
+	_putchar(j);
+	}
+	_putchar(10);
+	i++;
+	}
 }
