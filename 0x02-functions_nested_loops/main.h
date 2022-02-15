3da9347259525f2e303019e878898563a4bf4926
sevajal <sevajal@yahoo.com>
2022-02-15 08:57:37 -0500

    I did some changes

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,32 +9,9 @@
  *  
  */
 
-int _putchar(char c)
-
-{
-	return (write(1, &c, 1));
-}
-
-void print_alphabet(void)
-{
-	char i;
-
-	for (i = 97; i <= 122; i++)
-	_putchar(i);
-}
-
-void print_alphabet_x10(void)
-{
-	char i = 48;
-
-	while (i <= 57)
-	{
-	print_alphabet();
-	_putchar(10);
-	i++;
-	}
-}
-
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
 int _islower(int c);
 
 #endif
