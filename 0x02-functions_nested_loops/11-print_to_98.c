95367173ab1e42d35895163d565b769f3cccdaaf
sevajal <sevajal@yahoo.com>
2022-02-15 22:47:00 -0500

    I changed the code to printf

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - prints int until 98
+ * @n: it is the int to start from
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%i", i);
+			if (i != 98)
+				printf(", ");
+		}
+	}
+	else if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%i", i);
+			if (i != 98)
+				printf(", ");
+		}
+	}
+	printf("\n");
+}
