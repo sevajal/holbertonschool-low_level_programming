598b83a40464bb8add9cae36d9bc3a2936f5d062
sevajal <sevajal@yahoo.com>
2022-02-15 22:35:02 -0500

    I fixed acording with betty

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,14 @@
 #include "main.h"
 /**
  * add - prints the sum of two int.
+ * @num1: the first int
+ * @num2: the second int
+ * Return: sum
  */
 
 int add(int num1, int num2)
 {
-    int sum = (num1 + num2);
-    
+	int sum = (num1 + num2);
+
 	return (sum);
 }
