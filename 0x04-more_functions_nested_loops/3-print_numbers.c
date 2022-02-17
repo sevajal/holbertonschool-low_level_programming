5a846444a8bd422ed2804cd225b43d695461a423
sevajal <sevajal@yahoo.com>
2022-02-16 20:31:23 -0500

    I created the file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - prints the numbers from 0 to 9.
+ * Return: sum
+ */
+
+void print_numbers(void)
+{
+	int i = 48;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
