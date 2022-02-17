6040ece9b8c82aa2732337e123622669c93c8ae7
sevajal <sevajal@yahoo.com>
2022-02-17 09:54:32 -0500

    I removed the final space

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,7 +13,12 @@ int main(void)
 		if (((i % 5) == 0) && ((i % 3) == 0))
 			printf("FizzBuzz ");
 		else if ((i % 5) == 0)
-			printf("Buzz ");
+		{
+			if (i == 100)
+				printf("Buzz");
+			else
+				printf("Buzz ");
+		}
 		else if ((i % 3) == 0)
 			printf("Fizz ");
 		else
