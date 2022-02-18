90e7ce81817bebda013f18a7f3562f0ee8ecfe0e
sevajal <sevajal@yahoo.com>
2022-02-18 15:57:28 -0500

    I fixed according with betty

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,14 +7,15 @@ int main(void)
 {
 	long fib[50];
 	int i = 2;
+
 	fib[0] = 1;
 	fib[1] = 2;
-	
+
 	printf("%lu, ", fib[0]);
 	printf("%lu, ", fib[1]);
 	for (i = 2; i < 50; i++)
 	{
-		if(i != 49)
+		if (i != 49)
 		{
 		fib[i] = fib[i - 1] + fib[i - 2];
 		printf("%lu, ", fib[i]);
