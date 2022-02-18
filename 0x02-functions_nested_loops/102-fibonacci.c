31472cb973300ac9d1132d2c42213b6e27b7f823
sevajal <sevajal@yahoo.com>
2022-02-18 15:54:58 -0500

    I did some changes

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - prints the first 50 fibonnaci numbers
+ * Return: 0
+ */
+int main(void)
+{
+	long fib[50];
+	int i = 2;
+	fib[0] = 1;
+	fib[1] = 2;
+	
+	printf("%lu, ", fib[0]);
+	printf("%lu, ", fib[1]);
+	for (i = 2; i < 50; i++)
+	{
+		if(i != 49)
+		{
+		fib[i] = fib[i - 1] + fib[i - 2];
+		printf("%lu, ", fib[i]);
+		}
+		else
+			printf("%lu", fib[i]);
+	}
+	printf("\n");
+}
