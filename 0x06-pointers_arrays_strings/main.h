b92a7acb84f8bf3271b47ca7e470481ba1e3f539
sevajal <sevajal@yahoo.com>
2022-02-23 14:27:45 -0500

    I created the directory, the readme file and the main.h

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <unistd.h>
+
+/**
+ * Includes all my prototypes
+ * @c: The character to print
+ * @n: The int to use.
+ * @a: The int to use.
+ * @s2: The string 2.
+ * @s1: The string 1.
+ * @dest: String destination.
+ * @src: String source.
+ */
+
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+
+#endif
