1a6be3505115f663c24ac3cf303678f7c5866905
sevajal <sevajal@yahoo.com>
2022-02-23 16:49:24 -0500

    I created the file

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncpy - copies a string
+ * @dest: destination.
+ * @src: source.
+ * @n: numbers of bits
+ * Return: the pointer to dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	for (i = 0; src[i]; i++)
+		;
+	if (i >= n)
+	{
+		for (j = 0; j < n; j++)
+			dest[j] = src[j];
+	}
+	else if (i <= n)
+	{
+		for (j = 0; j <= i; j++)
+			dest[j] = src[j];
+	}
+	return (dest);
+}
