d29e7d2b160a5afff694de2aa58114f5301cff06
sevajal <sevajal@yahoo.com>
2022-02-24 16:49:28 -0500

    I changed the code

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,15 +9,11 @@
 char *cap_string(char *s)
 {
 	int i = 0;
-	char *p;
 
-	p = s;
 	for (i = 1; s[i]; i++)
 	{
 		if ((s[i - 1] < 48 || s[i - 1] > 122) && (s[i] >= 97 && s[i] <= 122))
-			p[i] = (s[i] - 32);
-		else
-			p[i] = (s[i]);
+			s[i] = (s[i] - 32);
 	}
 	return (s);
 }
