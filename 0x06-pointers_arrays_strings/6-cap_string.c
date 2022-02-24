98644c543aa3edd8968af480a4ce02381440fc27
sevajal <sevajal@yahoo.com>
2022-02-24 17:24:16 -0500

    I changed the code

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,6 +12,8 @@ char *cap_string(char *s)
 
 	for (i = 0; s[i]; i++)
 	{
+		if (s[i] == '\t')
+			s[i] = ' ';
 		if ((s[i - 1] < 48 || s[i - 1] > 122) && (s[i] >= 97 && s[i] <= 122))
 			s[i] = (s[i] - 32);
 	}
