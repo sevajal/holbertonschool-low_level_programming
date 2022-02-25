3fd9bde43a524a64f5da1b84fb1d7422ee2da64f
sevajal <sevajal@yahoo.com>
2022-02-25 15:11:25 -0500

    I changed the code

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,39 +8,29 @@
 
 void print_number(int n)
 {
-	int m;
-
-	if (n >= 0 && n <= 9)
-		_putchar(n + '0');
-	else if (n >= 10 && n <= 99)
+	if (n < 0)
 	{
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
+		_putchar('-');
+		n = -n;
 	}
-	else if (n >= 100 && n <= 999)
-	{
-		_putchar(n / 100 + '0');
-		_putchar((n / 10) % 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-	else if (n >= 1000 && n <= 9999)
+	if (n / 1000)
 	{
 		_putchar(n / 1000 + '0');
 		_putchar((n / 100) % 10 + '0');
 		_putchar((n / 10) % 10 + '0');
 		_putchar(n % 10 + '0');
 	}
-	else if (n < 0)
-		m = n * -1;
-	if (m > 0 && m <= 9)
+	else if (n / 100)
 	{
-		_putchar('-');
-		_putchar(m + '0');
+		_putchar(n / 100 + '0');
+		_putchar((n / 10) % 10 + '0');
+		_putchar(n % 10 + '0');
 	}
-	else if (m >= 10 && m <= 99)
+	else if (n / 10)
 	{
-		_putchar('-');
-		_putchar(m / 10 + '0');
-		_putchar(m % 10 + '0');
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
 	}
+	else if (n < 10)
+		_putchar(n + '0');
 }
