4462a5bdff1a08d6e28e5ed078ca309588bee160
sevajal <sevajal@yahoo.com>
2022-03-01 10:54:19 -0500

    I changed the code

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,37 +8,24 @@
 
 void print_number(int n)
 {
+	unsigned int i, abs, tmp;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		i = n * -1;
 	}
-	if (n / 10000)
+	else
+		i = n;
+
+	abs = i;
+	tmp = 1;
+
+	while (abs > 9)
 	{
-		_putchar(n / 10000 + '0');
-		_putchar((n / 1000) % 10 + '0');
-		_putchar((n / 100) % 10 + '0');
-		_putchar((n / 10) % 10 + '0');
-		_putchar(n % 10 + '0');
+		abs /= 10;
+		tmp *= 10;
 	}
-	else if (n / 1000)
-	{
-		_putchar(n / 1000 + '0');
-		_putchar((n / 100) % 10 + '0');
-		_putchar((n / 10) % 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-	else if (n / 100)
-	{
-		_putchar(n / 100 + '0');
-		_putchar((n / 10) % 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-	else if (n / 10)
-	{
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-	else if (n < 10)
-		_putchar(n + '0');
+	for (; tmp >= 1; tmp /= 10)
+		_putchar(((i / tmp) % 10) + '0');
 }
