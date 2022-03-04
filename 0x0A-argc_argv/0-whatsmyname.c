307eb6edfa00bd6d920adef37a301ee58b5e7662
sevajal <sevajal@yahoo.com>
2022-03-04 10:49:41 -0500

    I created the first files

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h> 
+
+/**
+ * main - return the name of the file.
+ * @argc: the number of arguments of the file.
+ * @argv: the array of arguments.
+ * Return: 0.
+ */
+
+int main (int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <unistd.h>
+
+/**
+ * Includes all my prototypes for argc and argv.
+ * @c: Character.
+ * @argc: the number of arguments of the file.
+ * @argv: the array of arguments.
+ */
+
+int _putchar(char c);
+int main (int __attribute__((unused)) argc, char *argv[]);
+
+#endif
