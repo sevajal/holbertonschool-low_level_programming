3e184c69b9c186df70a702bd9f13d517dfa86129
sevajal <sevajal@yahoo.com>
2022-03-08 12:18:48 -0500

    I changed the code

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 	char *array;
 
-	array = malloc(size * sizeof(array));
+	array = (char *)malloc(size * sizeof(char));
 
 	if (array == NULL || size == 0)
 		return (NULL);
