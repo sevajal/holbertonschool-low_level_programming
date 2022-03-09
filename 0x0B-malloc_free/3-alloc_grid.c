a28c1a867be7c88fe33c18df32570dc8582f5053
sevajal <sevajal@yahoo.com>
2022-03-08 21:53:47 -0500

    I changed the code

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -22,7 +22,10 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 
 	for (i = 0; i < height; i++)
+	{
+		grid[i] = 0;
 		grid[i] = malloc((width) * sizeof(int));
+	}
 
 	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
