a6adab58cca015d5ebd9a288c354d7dbb8469df5
sevajal <sevajal@yahoo.com>
2022-03-11 08:29:46 -0500

    I changed the code

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -23,19 +23,21 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	nconcat = malloc(sizeof(char) * (sizeof(s1) + n));
+	nconcat = malloc(sizeof(char) * (sizeof(s1) + n - 2));
 
 	if (nconcat == NULL)
 		return (NULL);
 
 	for (i = 0; s1[i]; i++)
 		nconcat[i] = s1[i];
-		for (j = 0; j < n; j++)
-		{
-			nconcat[i] = s2[j];
-			i++;
-			if (s2[j] == '\0')
-				break;
-		}
+
+	for (j = 0; j < n; j++)
+	{
+		nconcat[i] = s2[j];
+		i++;
+		if (s2[j] == '\0')
+			break;
+	}
+
 	return (nconcat);
 }
