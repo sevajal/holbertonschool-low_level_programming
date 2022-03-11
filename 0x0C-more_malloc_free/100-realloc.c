c0d786c2055fcc9398939574f19d82b46f4d6ddf
sevajal <sevajal@yahoo.com>
2022-03-11 14:09:07 -0500

    I fixed the code

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -22,7 +22,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 
-	if (new_size > old_size)
+	if (new_size > old_size || new_size < old_size)
 	{
 		realloc =  malloc(new_size * sizeof(new_size));
 
