08a4ddab5ad192ea1f14201ffb8c0b2f79f156c8
sevajal <sevajal@yahoo.com>
2022-03-14 18:54:42 -0500

    I changed the code

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,6 +3,21 @@
 #include "dog.h"
 
 /**
+ * _strlen - returns the length of a string.
+ * @s: the string.
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	for (i = 0; s[i]; i++)
+		;
+	return (i);
+}
+
+/**
  * new_dog -  creates a new dog
  * @name: dog's name
  * @age: dog's age
@@ -13,18 +28,45 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog2;
+	int namelen = _strlen(name);
+	int ownerlen = _strlen(owner);
+	int i;
 
 	dog2 = malloc(sizeof(dog_t));
 
-	if (dog2 == NULL || name == NULL || owner == NULL)
+	if (dog2 == NULL)
 	{
 		free(dog2);
 		return (NULL);
 	}
 
-	dog2->name = name;
+	dog2->name = malloc((namelen + 1)* sizeof(char));
+	if (name == NULL)
+	{
+		free(name);
+		return (NULL);
+	}
+	
+	for (i = 0; name[i] != '\0'; i++)
+	{
+		dog2->name[i] = name[i];
+	}
+	dog2->name[i] = '\0';
+
 	dog2->age = age;
-	dog2->owner = owner;
+
+	dog2->owner = malloc((ownerlen + 1) * sizeof(char));
+	if (owner == NULL)
+	{
+		free(owner);
+		return (NULL);
+	}
+
+	for (i = 0; owner[i] != '\0'; i++)
+	{
+		dog2->owner[i] = owner[i];
+	}
+	dog2->owner[i] = '\0';
 
 	return (dog2);
 }
