eedba9de82485ce5fee040d77456335743bc4849
sevajal <sevajal@yahoo.com>
2022-03-14 17:56:27 -0500

    I changed the code

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,14 +15,13 @@ void print_dog(struct dog *d)
 
 	if (d->name == NULL)
 		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
 
-	if (d->age == 0)
-		printf("Age: (nil)\n");
+	printf("Age: %.6f\n", d->age);
 
 	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
-
-	printf("Name: %s\n", d->name);
-	printf("Age: %.6f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	else
+		printf("Owner: %s\n", d->owner);
 }
