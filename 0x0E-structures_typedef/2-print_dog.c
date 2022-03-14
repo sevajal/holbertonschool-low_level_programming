e8928dbfcba7ed8fe16760d9d43a9da6d4cf4dac
sevajal <sevajal@yahoo.com>
2022-03-14 14:37:42 -0500

    I fixed the code

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,6 +10,15 @@
 
 void print_dog(struct dog *d)
 {
+	if(d == NULL)
+		exit(0);
+	
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+
 	printf("Name: %s\n", d->name);
 	printf("Age: %.6f\n", d->age);
 	printf("Owner: %s\n", d->owner);
