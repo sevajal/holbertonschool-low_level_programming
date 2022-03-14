6c1d96b4f16b13ebd7d1fc73fd0dbf1af47813ee
sevajal <sevajal@yahoo.com>
2022-03-14 11:16:14 -0500

    I created the file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#define NAME __FILE__
+/**
+ * main - prints the name of the file it was compiled from
+ * followed by a new line.
+ * Return: 0.
+ */
+
+int main(void)
+{
+	printf(NAME"\n");
+	return (0);
+}
