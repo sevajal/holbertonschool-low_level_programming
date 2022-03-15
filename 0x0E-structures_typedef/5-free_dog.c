450a6f5d9af6bb29fe4bc38556d91d9dabcce641
sevajal <sevajal@yahoo.com>
2022-03-14 20:17:07 -0500

    I created the code

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog -  frees dogs.
+ * @d: pointer to the struct
+ * Return: nothing.
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d);
+	free(d->owner);
+	free(d->name);
+}
