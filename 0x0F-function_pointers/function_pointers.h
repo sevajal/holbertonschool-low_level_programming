8b516349178441d01fceb34cb186444e36b9c471
sevajal <sevajal@yahoo.com>
2022-03-16 14:35:27 -0500

    I created the library

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,20 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+/**
+ * function_pointers - prototypes
+ * @name: name
+ * @f: pointer function
+ * @size: array size
+ * array: array
+ * @action: pointer function
+ * @cmp: pointer function
+ * @c: Character. 
+ */
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+int _putchar(char c);
+
+#endif
