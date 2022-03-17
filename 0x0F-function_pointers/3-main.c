d11ee5d4af5ff44f2a77dbbb56c0248c12de60bc
sevajal <sevajal@yahoo.com>
2022-03-17 11:35:48 -0500

    I created the codes

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,42 @@
+#include "3-calc.h"
+#include <stdio.h> 
+
+/**
+ * main - calculator function.
+ * @argc: int variable input, size argv
+ * @argv: pointer to char array input, contain arguments
+ * Return:
+ * if the number of arguments is wrong, print Error,
+ * followed by a new line, and exit with the status 98
+ * if the operator is none of the above, print Error,
+ * followed by a new line, and exit with the status 99
+ * if the user tries to divide (/ or %) by 0, print Error,
+ * followed by a new line, and exit with the status 100
+ */
+
+int main(int argc, char *argv[])
+{
+	int (*op)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	op = get_op_func(argv[2]);
+	if (!op)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%i\n", op(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
