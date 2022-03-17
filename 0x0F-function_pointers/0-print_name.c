bfa843723c5f72d8e88923f7564c407e21453afe
sevajal <sevajal@yahoo.com>
2022-03-17 10:45:22 -0500

    I changed the format

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,14 @@
 #include "function_pointers.h"
-#include <stdlib.h>
 
 /**
  * print_name - prints a name.
  * @name: the name to print.
- * @f: function pointer.
- * Return: void.
+ * @f: function to a pointer.
+ * Return: nothing.
  */
 
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
-		f(name);
+		(*f)(name);
 }
