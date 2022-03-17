652da41fc1d45037f7b68ae0ae47a2cd308cc977
sevajal <sevajal@yahoo.com>
2022-03-17 10:53:37 -0500

    I changed the code

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,12 @@
 #include "function_pointers.h"
 
 /**
- * print_name - function that prints a name.
- * @name: pointer of string
- * @f: funtion pointer
+ * print_name - prints a name.
+ * @name: the name to print.
+ * @f: function to a pointer.
  */
 void print_name(char *name, void (*f)(char *))
 {
 	if (f)
-	{
 		f(name);
-	}
 }
