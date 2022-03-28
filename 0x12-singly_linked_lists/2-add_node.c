559ae591bdb79cdf24efb34222f3ee1b6f02ea97
sevajal <sevajal@yahoo.com>
2022-03-28 12:22:09 -0500

    I created the code

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list.
+ * @head: double pointer to head.
+ * @str: pointer to the string in the struct.
+ * Return: the address of the new element, or NULL if it failed.
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	int i;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	for (i = 0; str[i]; i++)
+	new->len = i + 1;
+	new->next = *head;
+	*head = new;
+	return (*head);
+}
