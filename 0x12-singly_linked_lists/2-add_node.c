29d82eb591da63c179308f7348a8f8b93c07bc1f
sevajal <sevajal@yahoo.com>
2022-03-28 14:43:43 -0500

    I casted the malloc

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,7 +12,7 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new;
 	int i;
 
-	new = malloc(sizeof(list_t));
+	new = (list_t *)malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
 	new->str = strdup(str);
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -13,7 +13,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *last = *head;
 	int i;
 
-	new = malloc(sizeof(list_t));
+	new = (list_t *)malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
 	new->str = strdup(str);
