61da7fb4f8d52f595b8e66ec5fd017551beed7ff
sevajal <sevajal@yahoo.com>
2022-04-01 16:46:57 -0500

    I reduced the variables to two

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -9,17 +9,16 @@
 listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *prev = NULL;
-	listint_t *current = *head;
 	listint_t *next = NULL;
 
 	if (head || *head)
 	{
-		while (current != NULL)
+		while (*head != NULL)
 		{
-			next = current->next;
-			current->next = prev;
-			prev = current;
-			current = next;
+			next = (*head)->next;
+			(*head)->next = prev;
+			prev = *head;
+			*head = next;
 		}
 		*head = prev;
 		return (*head);
