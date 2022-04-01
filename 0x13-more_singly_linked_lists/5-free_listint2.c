48a76d482dd3c5d69097a2fc018b3822c475e7a3
sevajal <sevajal@yahoo.com>
2022-04-01 09:51:28 -0500

    I added a validation

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,18 +9,17 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *current = *head;
-	listint_t *next;
-
 	if (*head)
 	{
+		listint_t *current = *head;
+		listint_t *next;
+
 		while (current != NULL)
 		{
 			next = current->next;
 			free(current);
 			current = next;
 		}
-		free(next);
 		*head = NULL;
 	}
 }
