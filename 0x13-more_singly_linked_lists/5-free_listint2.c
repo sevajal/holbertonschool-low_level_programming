3e4f9de8b1c1d592a4245020803bb6eecf3d79e4
sevajal <sevajal@yahoo.com>
2022-04-01 10:16:35 -0500

    I changed the code

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,22 +3,21 @@
 /**
  * free_listint2 - frees a listint_t list.
  * @head: Double pointer to the listint_t head.
- * The function sets the head to NULL
+ * The function sets the head to NULL.
  * Return: nothing.
  */
 
 void free_listint2(listint_t **head)
 {
-	if (*head != NULL)
+	if (*head)
 	{
 		listint_t *current = *head;
-		listint_t *next;
 
 		while (current != NULL)
 		{
-			next = current->next;
+			*head = (*head)->next;
 			free(current);
-			current = next;
+			current = *head;
 		}
 		*head = NULL;
 	}
