3ede99cc556edc4c825324adfd8b2862aa0880ba
sevajal <sevajal@yahoo.com>
2022-04-01 15:07:37 -0500

    I added the condition next not null

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,7 +28,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 
 		for (i = 0; i < idx - 1; i++)
+		{
+			if (tmp->next == NULL)
+				return (NULL);
 			tmp = tmp->next;
+		}
 		if (tmp == NULL)
 			return (NULL);
 		new = (listint_t *)malloc(sizeof(listint_t));
