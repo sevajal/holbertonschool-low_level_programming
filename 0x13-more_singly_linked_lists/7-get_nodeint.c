9e7eb460aaa31c099e7dcd640e7d8c099b1b6fc3
sevajal <sevajal@yahoo.com>
2022-04-01 14:51:57 -0500

    I changed one condition

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,8 +16,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	{
 		for (i = 0; i < index; i++)
 		{
-			if (head->next != NULL)
-				head = head->next;
+			if (head->next == NULL)
+				return (NULL);
+			head = head->next;
 		}
 		if (head == NULL)
 			return (NULL);
