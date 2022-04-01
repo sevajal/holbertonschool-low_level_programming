35c946ba38281accb2a343240e0a57d922932194
sevajal <sevajal@yahoo.com>
2022-04-01 08:43:27 -0500

    I changed the return value back

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,7 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		new->n = n;
 		new->next = tmp->next;
 		tmp->next = new;
-		return (*head);
+		return (new);
 	}
 	else
 		return (NULL);
