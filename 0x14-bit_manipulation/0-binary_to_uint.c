d9289817134b55f256ff2f809bc8732859e3c48e
sevajal <sevajal@yahoo.com>
2022-04-05 11:50:30 -0500

    I added a new condition for null array

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,28 +10,28 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int i = 0;
-	unsigned int count = 0;
+	unsigned int num = 0;
 	unsigned int len;
 
-	if (b)
+	if (b || *b)
 	{
 		for (i = 0; b[i]; i++)
 			;
 		len = i;
-		count = b[0] - '0';
+		num = b[0] - '0';
 		if (len == 1)
-			return (count);
+			return (num);
 		for (i = 0; b[i]; i++)
 		{
 			if (b[i] == '0' || b[i] == '1')
 			{
 				if (b[i + 1] != '\0')
-					count = count * 2 + b[i + 1] - '0';
+					num = num * 2 + b[i + 1] - '0';
 			}
 			else
 				return (0);
 		}
-		return (count);
+		return (num);
 	}
 	return (0);
 }
