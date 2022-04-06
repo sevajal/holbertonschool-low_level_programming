91dc63bc8d0805543c97fd0e746cea97237e1ff6
sevajal <sevajal@yahoo.com>
2022-04-05 21:55:49 -0500

    I changed the flags in open

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,9 +20,11 @@ int _strlen(char *s)
  * @filename: the name of the file to create.
  * @text_content: a NULL terminated string to write to the file.
  * Return: 1 on success, -1 on failure.
- * if the file can not be opened or read, return 0.
- * if filename is NULL return 0.
- * if write fails or does not write the expected amount of bytes, return 0.
+ * The created file must have those permissions: rw-------.
+ * If the file already exists, do not change the permissions.
+ * if the file already exists, truncate it.
+ * if filename is NULL return -1.
+ * if text_content is NULL create an empty file.
  */
 
 int create_file(const char *filename, char *text_content)
@@ -33,7 +35,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_RDWR, 00600 | O_TRUNC);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 00600);
 	if (fd == -1)
 		return (-1);
 
