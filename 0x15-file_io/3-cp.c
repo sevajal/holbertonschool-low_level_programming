b964ded977ff9c64660aba98b19f30851b02e66e
sevajal <sevajal@yahoo.com>
2022-04-06 17:47:39 -0500

    I changed the 2 for STDERR

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,38 +13,38 @@ int main(int ac, char **av)
 	char buffer[1024];
 
 	if (ac != 3)
-	{	dprintf(2, "Usage: cp file_from file_to\n");
+	{	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97); }
 	fd_from = open(av[1], O_RDONLY);
 	if (fd_from == -1)
-	{	dprintf(2, "Error: Can't read from file %s\n", av[1]);
+	{	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98); }
 	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 00664);
 	if (fd_to == -1)
-	{	dprintf(2, "Error: Can't write to %s\n", av[2]);
+	{	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99); }
 	valRead = 1024;
 	while (valRead > 0)
 	{
 		valRead = read(fd_from, buffer, 1024);
 		if (valRead == -1)
-		{	dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		{	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 			exit(98); }
 		valWrite = write(fd_to, buffer, valRead);
 		if (valWrite == -1 || valRead != valWrite)
-		{	dprintf(2, "Error: Can't write to %s\n", av[2]);
+		{	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 			exit(99); }
 	}
 	valClose_from = close(fd_from);
 	if (valClose_from == -1)
 	{
-		dprintf(2, "Error: Can't close fd %ld\n", fd_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %ld\n", fd_from);
 		exit(100);
 	}
 	valClose_to = close(fd_to);
 	if (valClose_to == -1)
 	{
-		dprintf(2, "Error: Can't close fd %ld\n", fd_to);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %ld\n", fd_to);
 		exit(100);
 	}
 	return (1);
