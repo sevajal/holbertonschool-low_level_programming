676f8456fac6431eb8a5697ebf78a0a2e57d6907
sevajal <sevajal@yahoo.com>
2022-05-12 10:01:47 -0500

    Back

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,7 +13,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i = 0;
 	dlistint_t *new, *tmp = *h;
 
-	if (h && *h && idx)
+	if (h && *h)
 	{
 		new = (dlistint_t *)malloc(sizeof(dlistint_t));
 		if (new == NULL)
