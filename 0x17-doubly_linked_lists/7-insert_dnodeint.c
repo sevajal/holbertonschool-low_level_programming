46e7aecddd3e96311048041130e79ae63a52c598
sevajal <sevajal@yahoo.com>
2022-05-12 10:05:03 -0500

    If index negative do as 0

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -20,7 +20,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			return (NULL);
 		new->n = n;
 
-		if (idx == 0)
+		if (idx < 1)
 		{
 			new->next = *h;
 			new->prev = NULL;
