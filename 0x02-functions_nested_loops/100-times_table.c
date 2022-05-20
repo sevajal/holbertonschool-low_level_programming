1c0b82079acfffd9ae9c9bdb49e4bed3dcdfc684
sevajal <sevajal@yahoo.com>
2022-05-20 14:57:13 -0500

    We finished the code

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
  * print_times_table - prints the multiplication tables (1-15)
+ * @n: int.
+ * Return: nothing.
  */
 
 void print_times_table(int n)
@@ -13,57 +15,32 @@ void print_times_table(int n)
 	{
 		while (i <= n)
 		{
-			j = 0;
+			_putchar('0');
+			j = 1;
 			while (j <= n)
 			{
-				a = ((i) * (j));
-				if (a >= 0 && a <= 9)
+				a = i * j;
+				_putchar(',');
+				_putchar(' ');
+				if (a <= 9)
 				{
+					_putchar(' ');
+					_putchar(' ');
 					_putchar(a + '0');
-					if (j != n)
-					{
-						_putchar(',');
-						if ((i * (j + 1)) > 9)
-						{
-							_putchar(' ');
-							_putchar(' ');
-						}
-						else
-						{
-							_putchar(' ');
-							_putchar(' ');
-							_putchar(' ');
-						}
-					}
 				}
-				else if (a >= 10 && a <= 99)
+				else if (a <= 99)
 				{
+					_putchar(' ');
 					_putchar((a / 10) + '0');
 					_putchar((a % 10) + '0');
-					if (j != n)
-					{
-						_putchar(',');
-						if ((i * (j + 1)) > 99)
-							_putchar(' ');
-						else
-						{
-						_putchar(' ');
-						_putchar(' ');
-						}
-					}
 				}
-				else if (a >= 100 && a <= 999)
+				else
 				{
 					_putchar((a / 100) + '0');
 					_putchar(((a / 10) % 10) + '0');
 					_putchar((a % 10) + '0');
-					if (j != n)
-					{
-						_putchar(',');
-						_putchar(' ');
-					}
 				}
-				j++;
+			j++;
 			}
 			_putchar('\n');
 			i++;
