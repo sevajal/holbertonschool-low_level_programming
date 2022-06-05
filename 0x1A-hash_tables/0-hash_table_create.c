c22392a35a32792392d4eccf3dd91d24319fef44
sevajal <sevajal@yahoo.com>
2022-06-05 00:31:47 -0500

    Checking for calloc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,14 +11,17 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
 	hash_node_t **array;
+	unsigned long int i = 0;
 
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
-	array = calloc(size, sizeof(hash_node_t *));
+	array = malloc(size * sizeof(hash_node_t *));
 	if (array == NULL)
 		return (NULL);
 	ht->size = size;
 	ht->array = array;
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
 	return (ht);
 }
