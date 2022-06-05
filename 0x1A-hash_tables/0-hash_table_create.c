aeebe5c8eacbf09c21f2806691d873575571677a
sevajal <sevajal@yahoo.com>
2022-06-04 21:49:16 -0500

    Betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,7 +2,7 @@
 
 /**
  * hash_table_create - creates a hash table.
- * @size: size of the array 
+ * @size: size of the array.
  * Return: a pointer to the newly created hash table
  * or NULL if something went wrong.
  */
