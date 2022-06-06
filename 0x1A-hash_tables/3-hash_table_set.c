3353c466335251bd5d677c34b5478b38e0f2b765
sevajal <sevajal@yahoo.com>
2022-06-05 19:58:30 -0500

    Betty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,43 +16,39 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new;
 	hash_node_t *tmp;
 
-	if (ht)
+	if (!ht || !key)
+		return (0);
+	index = key_index((unsigned char *)key, ht->size);
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (0);
+	new->key = strdup(key);
+	new->value = strdup(value);
+	new->next = NULL;
+	tmp = ht->array[index];
+	if (!tmp)
 	{
-		if (!key)
-			return (0);
-		index = key_index((unsigned char *)key, ht->size);
-		new = malloc(sizeof(hash_node_t));
-		if (new == NULL)
-			return (0);
-		new->key = strdup(key);
-		new->value = strdup(value);
-		new->next = NULL;
-		tmp = ht->array[index];
-		if (!tmp)
-		{
-			ht->array[index] = new;
-			return (1);
-		}
-		else
-		{
-			while (tmp)
-			{
-				if (strcmp(tmp->key, key) == 0)
-				{
-					free(tmp->value);
-					tmp->value = strdup(value);
-					free(new->key);
-					free(new->value);
-					free(new);
-					return (1);
-				}
-				tmp = tmp->next;
-			}
-			tmp = ht->array[index];
-			new->next = tmp;
-			ht->array[index] = new;
+		ht->array[index] = new;
 		return (1);
-		}
 	}
-	return (0);
+	else
+	{
+		while (tmp)
+		{
+			if (strcmp(tmp->key, key) == 0)
+			{
+				free(tmp->value);
+				tmp->value = strdup(value);
+				free(new->key);
+				free(new->value);
+				free(new);
+				return (1);
+			}
+			tmp = tmp->next;
+		}
+		tmp = ht->array[index];
+		new->next = tmp;
+		ht->array[index] = new;
+	return (1);
+	}
 }
